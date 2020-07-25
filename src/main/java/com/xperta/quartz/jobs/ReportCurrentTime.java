23aaf5ada258fd86b67cc29e95639f1f84613b4c
YusufKolcuk <37012068+YusufKolcuk@users.noreply.github.com>
2020-07-25 23:56:06 +0300

    spring-quartz-integration

--- src/main/java/com/xperta/quartz/jobs/ReportCurrentTime.java
+++ src/main/java/com/xperta/quartz/jobs/ReportCurrentTime.java
@@ -1,18 +1,23 @@
 package com.xperta.quartz.jobs;
 
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
 
-public class WeatherDataCall implements Job{
+@Component("reportCurrentTime")
+public class ReportCurrentTime
+{
 
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		// TODO Auto-generated method stub
-		String urlParameters=null;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "HH:mm:ss");
+
+    public void printCurrentTime()
+    {
+    	String urlParameters=null;
 		try {
 			urlParameters = "fName=" + URLEncoder.encode("???", "UTF-8") +
 			"&lName=" + URLEncoder.encode("???", "UTF-8");
@@ -21,7 +26,8 @@ public class WeatherDataCall implements Job{
 			e.printStackTrace();
 		}
 		WeatharDataJob.executePost("http://api.openweathermap.org/data/2.5/group?id=4099974&appid=294fcf03ced531801723f6d093726c4c&units=metric", urlParameters);
-	}
-	
+        System.out.println(
+                "Current time = " + dateFormat.format(new Date()));
+    }
 
 }
