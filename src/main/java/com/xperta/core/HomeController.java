f63478c436e652d017503fed8afe8f6b1c6bfc6c
user <user@DESKTOP-JDJDM0U>
2020-08-17 16:20:48 +0300

    sc

--- src/main/java/com/xperta/core/HomeController.java
+++ src/main/java/com/xperta/core/HomeController.java
@@ -2,6 +2,9 @@
 
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +14,11 @@ import org.springframework.web.servlet.ModelAndView;
 import com.xperta.entity.City;
 import com.xperta.service.CitiesService;
 
-
-
 @Controller
 public class HomeController {
+
 	
+	private static final Logger Log=LoggerFactory.getLogger(HomeController.class);
 	public static String url = "http://localhost:8080/core/";
 	@Autowired
 	private CitiesService citiesService;
@@ -37,7 +40,7 @@ public class HomeController {
 		List<City> listEmp = citiesService.getAllCitiess();
 		model.addObject("listEmp", listEmp);
 		model.setViewName("table");
-		
+		Log.info("trial 1 ");
 		
 		return model;
 	}
