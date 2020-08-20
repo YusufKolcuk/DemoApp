076d79fd403414757d9ae55d20dcf144ab74093d
YusufKolcuk <HNR@192.168.1.133>
2020-08-20 13:51:48 +0300

    m

--- src/main/java/com/xperta/core/HomeController.java
+++ src/main/java/com/xperta/core/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	 @Autowired
 	 PDFJService pdfService;
 
-	//testtt
+	//Yusuf Kolcuk 6
 	private static final Logger Log=LoggerFactory.getLogger(HomeController.class);
 	public static String url = "http://localhost:8080/core/";
 	@Autowired
