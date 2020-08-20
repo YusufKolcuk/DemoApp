e1369a6472e67119475506d9bbf715cd64c24908
YusufKolcuk <yusuf.kolcuk.44@gmail.com>
2020-08-20 12:25:52 +0300

    git test3

--- src/main/java/com/xperta/core/LoginController.java
+++ src/main/java/com/xperta/core/LoginController.java
@@ -18,7 +18,7 @@ import com.xperta.entity.User;
 import com.xperta.service.CitiesService;
 import com.xperta.service.UserService;
 
-//Cem Kucuk
+//Cem Kucuk 2
 @Controller
 public class LoginController {
 
